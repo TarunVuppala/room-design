ceff1dead643b9cebe60cd61808665f3180213c1
TarunVuppala <tarun.vuppala26@gmail.com>
2023-07-14 23:36:24 +0530

    This is the First Commit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
- import { Canvas } from 'react-three-fiber';
 
 // import Model from './modal';
 
